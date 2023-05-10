320ac0edc4cf70d7034e3672acab917bf26efc39
Aurtekh <ermolovichd007@gmail.com>
2023-05-10 04:22:40 +0300

    add login and registration

---
+++ index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import { registerValidation } from './validations/auth.js';
+
+import checkAuth from './utils/checkAuth.js';
+
+import * as UserController from './controllers/UserController.js';
+
+mongoose
+  .connect(
+    'mongodb+srv://admin:ReactBlog007@cluster0.bmhpdht.mongodb.net/socialNetwork?retryWrites=true&w=majority',
+  )
+  .then(() => console.log('DB ok'))
+  .catch(() => console.log('DB error'));
+
+const app = express();
+
+app.use(express.json());
+
+app.post('/auth/login', UserController.login);
+
+app.post('/auth/register', registerValidation, UserController.register);
+
+app.get('/auth/me', checkAuth, UserController.getMe);
+
+app.listen(4444, (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  console.log('server OK');
+});
